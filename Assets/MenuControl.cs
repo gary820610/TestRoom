c9e3e62d84ff09d40a05c14ce873d063b620cee1
Asato <asato_chen@outlook.com>
2020-07-06 00:17:37 +0800

    搞懂了unity touch類deltaPosition的疑似bug
    deltaPostion有時並不會直接反應在touch.position，明明前後幀position數值相同，deltaPosition卻不是零且與上一幀deltaPos數值完全相同，因此純用deltaPosition來計算位移，物體實際上會跑得比手指更快(位移量更大)。可能是unity的bug

--- Assets/MenuControl.cs
+++ Assets/MenuControl.cs
@@ -23,6 +23,8 @@ public class MenuControl : MonoBehaviour
     float _validSpeed = 0.05f;
     float _validDist = 1.0f;
 
+    Direction _dir;
+    Vector2 _touchOffSet;
 
 
     // Start is called before the first frame update
@@ -33,6 +35,7 @@ public class MenuControl : MonoBehaviour
         m_Cam = GameObject.FindGameObjectWithTag("MainCamera"); ;
         _camCon = m_Cam.GetComponent<CameraController>();
         _viewNum = 0;
+        _dir = Direction.NODIRECTION;
     }
 
     // Update is called once per frame
@@ -47,25 +50,57 @@ public class MenuControl : MonoBehaviour
         {
             case TouchPhase.Began:
                 _touchPointStart = Input.GetTouch(0).position;
+                _touchOffSet = new Vector2(Screen.width / 2, Screen.height / 2) - _touchPointStart;
                 break;
             case TouchPhase.Moved:
                 var deltaPos = Input.GetTouch(0).deltaPosition;
-                if (Mathf.Abs(deltaPos.x) >= Mathf.Abs(deltaPos.y))
-                {
-                    var moveVec = (deltaPos.x / Screen.width) * _view.rect.width;
-                    _view.anchoredPosition += new Vector2(moveVec, 0);
-                }
-                else
-                {
-                    var moveVec = (deltaPos.y / Screen.height) * _view.rect.height;
-                    _view.anchoredPosition += new Vector2(0, moveVec);
-                }
+                var newPos = new Vector2(Input.GetTouch(0).position.x - (Screen.width / 2), Input.GetTouch(0).position.y - (Screen.height / 2));
+                var moveVec = deltaPos.y * _view.rect.height / Screen.height;
+                var newPos2 = _pages.anchoredPosition + new Vector2(0, moveVec);
+                _pages.anchoredPosition = newPos + _touchOffSet;
+
+                Debug.Log("___delta : " + deltaPos);
+                Debug.Log("newPos : " + newPos);
+                Debug.Log("newPos2 : " + newPos2);
+
+                // if (_dir == Direction.NODIRECTION)
+                // {
+                //     if (Mathf.Abs(deltaPos.x) >= Mathf.Abs(deltaPos.y))
+                //     {
+                //         _dir = Direction.HORIZONTAL;
+                //         var moveVec = deltaPos.x * _view.rect.width / Screen.width;
+                //         _pages.anchoredPosition += new Vector2(moveVec, 0);
+                //     }
+                //     else
+                //     {
+                //         _dir = Direction.VERTICAL;
+                //         var moveVec = deltaPos.y * _view.rect.height / Screen.height;
+                //         _totalDist += deltaPos.y;
+                //         _pages.anchoredPosition += new Vector2(0, moveVec);
+                //     }
+                // }
+
+                // if (_dir == Direction.HORIZONTAL)
+                // {
+                //     var moveVec = deltaPos.x * _view.rect.width / Screen.width;
+                //     _pages.anchoredPosition += new Vector2(moveVec, 0);
+                // }
+                // else if (_dir == Direction.VERTICAL)
+                // {
+                //     var moveVec = deltaPos.y * _view.rect.height / Screen.height;                //
+
+                //     Debug.Log("moveVec" + moveVec);
+                //     Debug.Log("deltaPos.y" + deltaPos.y);
+                //     _pages.anchoredPosition += new Vector2(0, moveVec);
+                // }
+
                 break;
             case TouchPhase.Ended:
                 _touchPointEnd = Input.GetTouch(0).position;
-                _moveDist = _touchPointEnd.x - _touchPointStart.x;
+                _moveDist = _touchPointEnd.y - _touchPointStart.y;
                 _moveSpeed = Input.GetTouch(0).deltaPosition.x;
-
+                // Debug.Log("_moveDist : " + _moveDist);
+                // Debug.Log("_pages.anchoredPosition" + _pages.anchoredPosition);
                 // if (Mathf.Abs(_moveSpeed) < _validSpeed)
                 //     _moveSpeed = 0f;
                 // if (_moveSpeed * _moveDist < 1)
@@ -73,6 +108,8 @@ public class MenuControl : MonoBehaviour
                 // if (Mathf.Abs(_moveDist) < _validDist)
                 //     _moveSpeed = 0f;
                 // Slide(_moveSpeed);
+                _dir = Direction.NODIRECTION;
+                _pages.anchoredPosition = new Vector2(0, 0);
                 break;
         }
     }
@@ -86,4 +123,11 @@ public class MenuControl : MonoBehaviour
     {
 
     }
+}
+
+enum Direction
+{
+    NODIRECTION,
+    HORIZONTAL,
+    VERTICAL
 }
