911425524c9e40187c39341576c61d328a81118f
Asato <asato_chen@outlook.com>
2020-07-21 23:27:29 +0800

    新增海戰測試單元
    已完成 - 移動測試方案
    待完成 - 攻擊方案
                - 邊界限制方案

---
+++ Assets/Scripts/NavelBattle/NaviBattleController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NaviBattleController : MonoBehaviour
+{
+    [SerializeField]
+    ShipModel _playerShip;
+    Vector3 oriFingerPos;
+    Vector3 endFingerPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ControlShip();
+    }
+
+    void ControlShip()
+    {
+        if (Input.touchCount <= 0) return;
+
+        switch (Input.GetTouch(0).phase)
+        {
+            case TouchPhase.Began:
+                oriFingerPos = new Vector3(Input.GetTouch(0).position.x, Input.GetTouch(0).position.y, 10f);
+                Ray ray = Camera.main.ScreenPointToRay(oriFingerPos);
+                RaycastHit hit;
+                Physics.Raycast(ray, out hit, 1000);
+                if (hit.collider == null || hit.collider.gameObject.tag != "Player")
+                {
+                    Debug.Log("No collider hit");
+                    return;
+                }
+                else
+                {
+                    _playerShip.ShowEffect(true);
+                }
+                break;
+            case TouchPhase.Moved:
+                break;
+            case TouchPhase.Ended:
+                endFingerPos = new Vector3(Input.GetTouch(0).position.x, Input.GetTouch(0).position.y, 10f);
+                oriFingerPos = Camera.main.ScreenToWorldPoint(oriFingerPos);
+                endFingerPos = Camera.main.ScreenToWorldPoint(endFingerPos);
+                var forwardVec = (endFingerPos - oriFingerPos).normalized * _playerShip.GetShipLength();
+                _playerShip.MoveTo(forwardVec);
+                _playerShip.ShowEffect(false);
+                break;
+        }
+    }
+}
---
+++ Assets/Scripts/NavelBattle/ShipModel.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipModel : MonoBehaviour
+{
+    [SerializeField]
+    GameObject _shipPrefab;
+    [SerializeField]
+    GameObject _particleSys;
+    Vector3 _targetPos;
+    [SerializeField]
+    MovePlan _movingPlan;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ShowEffect(false);
+        _targetPos = this.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    public float GetShipLength()
+    {
+        // return _shipPrefab.transform.localScale.z;
+        return 20f;
+    }
+
+    public void ShowEffect(bool state)
+    {
+        _particleSys.SetActive(state);
+    }
+
+    public void MoveTo(Vector3 forwardVector)
+    {
+        forwardVector.y = 0;
+        _targetPos = this.transform.position + forwardVector;
+    }
+
+    public void Move()
+    {
+        float distance = Vector3.Distance(this.transform.position, _targetPos);
+        Vector3 direction = Vector3.Normalize(_targetPos - this.transform.position);
+        float angle = Vector3.Angle(this.transform.forward, direction);
+        if (Vector3.Cross(this.transform.forward, direction).y < 0) angle *= -1;
+
+        switch (_movingPlan)
+        {
+            case MovePlan.PLAN_A:
+                if (Mathf.Abs(angle) > 0.2)
+                {
+                    this.transform.Rotate(0, angle * 0.02f, 0);
+                }
+                else if (distance > 3)
+                {
+                    this.transform.position = Vector3.Lerp(this.transform.position, _targetPos, 0.01f);
+                }
+                break;
+            case MovePlan.PLAN_B:
+                if (Mathf.Abs(angle) > 0.2)
+                {
+                    this.transform.Rotate(0, angle * 0.02f, 0);
+                }
+                if (distance > 3)
+                {
+                    this.transform.position += this.transform.forward * 0.1f;
+                }
+                break;
+        }
+
+        if (Mathf.Abs(angle) > 0.1)
+        {
+            this.transform.Rotate(0, angle * 0.02f, 0);
+        }
+        else if (distance > 3)
+        {
+
+        }
+    }
+}
+
+enum MovePlan
+{
+    PLAN_A,
+    PLAN_B
+}
