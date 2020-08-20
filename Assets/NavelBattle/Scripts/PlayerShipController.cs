d4dec7d1489b81b1202d896d46e86f5481355dcc
Asato <asato_chen@outlook.com>
2020-08-20 16:11:26 +0800

    變更海戰操作邏輯

--- Assets/NavelBattle/Scripts/PlayerShipController.cs
+++ Assets/NavelBattle/Scripts/PlayerShipController.cs
@@ -5,49 +5,29 @@ using UnityEngine.UI;
 
 public class PlayerShipController : MonoBehaviour
 {
+    [SerializeField]
     ShipModel _playerShip;
+    [SerializeField]
+    GameObject _pressEffect;
     Vector3 _oriFingerPos;
     Vector3 _endFingerPos;
-    [SerializeField]
-    Slider _powerBar;
 
-    float _timer = 0;
-    bool _isAiming = false;
+    int _timer;
+    int _maxFirePressTime = 30;
+    float _minMoveSlideLength = 100;
 
     // Start is called before the first frame update
     void Start()
     {
-        _playerShip = this.gameObject.GetComponent<ShipModel>();
+        //_playerShip = this.gameObject.GetComponent<ShipModel>();
+        _timer = 0;
+        _pressEffect.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
         ControlShip();
-        Aim();
-    }
-
-    public void StartAiming()
-    {
-        _isAiming = true;
-        _timer = 0;
-    }
-
-    public void FireLeftCannon()
-    {
-        _playerShip.FireCannon("L", _timer);
-        _isAiming = false;
-    }
-
-    public void FireRightCannon()
-    {
-        _playerShip.FireCannon("R", _timer);
-        _isAiming = false;
-    }
-
-    public float GetTimer()
-    {
-        return _timer;
     }
 
     void ControlShip()
@@ -65,39 +45,63 @@ public class PlayerShipController : MonoBehaviour
         {
             case TouchPhase.Began:
                 _oriFingerPos = new Vector3(Input.GetTouch(0).position.x, Input.GetTouch(0).position.y, 100f);
-                Ray ray = Camera.main.ScreenPointToRay(_oriFingerPos);
-                RaycastHit hit;
-                Physics.Raycast(ray, out hit, 1000);
-                if (hit.collider == null || hit.collider.gameObject.tag != "Player")
-                {
-                    Debug.Log("No collider hit");
-                    return;
-                }
-                else
-                {
-                    _playerShip.ShowEffect(true);
-                }
+                Vector3 fingerPos = Camera.main.ScreenToWorldPoint(_oriFingerPos);
                 break;
-            case TouchPhase.Moved:
+            case TouchPhase.Stationary:
+                _timer += 1;
+                if (_timer > 25)
+                {
+                    Vector3 fingerPosNow = Camera.main.ScreenToWorldPoint(_oriFingerPos);
+                    ShowEffect(true, fingerPosNow);
+                }
                 break;
             case TouchPhase.Ended:
                 _endFingerPos = new Vector3(Input.GetTouch(0).position.x, Input.GetTouch(0).position.y, 100f);
-                _oriFingerPos = Camera.main.ScreenToWorldPoint(_oriFingerPos);
-                _endFingerPos = Camera.main.ScreenToWorldPoint(_endFingerPos);
-                var forwardVec = (_endFingerPos - _oriFingerPos).normalized * _playerShip.GetShipLength();
-                _playerShip.MoveTo(forwardVec);
-                _playerShip.ShowEffect(false);
+                float dist = Vector3.Distance(_oriFingerPos, _endFingerPos);
+
+                if (_timer < _maxFirePressTime)
+                {
+                    Vector3 targetPos = Camera.main.ScreenToWorldPoint(_endFingerPos);
+                    Fire(targetPos);
+                }
+                else if (dist > _minMoveSlideLength)
+                {
+                    _oriFingerPos = Camera.main.ScreenToWorldPoint(_oriFingerPos);
+                    _endFingerPos = Camera.main.ScreenToWorldPoint(_endFingerPos);
+                    var forwardVec = (_endFingerPos - _oriFingerPos).normalized * _playerShip.GetShipLength();
+                    _playerShip.MoveTo(forwardVec);
+                }
+                _timer = 0;
+                ShowEffect(false, Vector3.zero);
                 break;
         }
     }
 
-    void Aim()
+    void ShowEffect(bool onOff, Vector3 fingerPos)
     {
-        _powerBar.value = _timer;
-        if (!_isAiming) return;
+        _pressEffect.SetActive(onOff);
+        fingerPos.y = _playerShip.gameObject.transform.position.y;
+        _pressEffect.transform.position = fingerPos;
+    }
 
-        if (_timer >= 1) _timer = 0;
-        _timer += Time.deltaTime;
-        _powerBar.value = _timer;
+    void Fire(Vector3 target)
+    {
+        _playerShip.FireCannon(target);
+        Debug.LogWarning("FIRE!!!!!!!!");
+    }
+
+    public override int GetHashCode()
+    {
+        return base.GetHashCode();
+    }
+
+    public override bool Equals(object other)
+    {
+        return base.Equals(other);
+    }
+
+    public override string ToString()
+    {
+        return base.ToString();
     }
 }
